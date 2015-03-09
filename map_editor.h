c9360d97d0606487e1cde68b290c945a77e771a5
leonardxo <leonardnguyen@Leonards-MacBook-Pro.local>
2015-03-09 11:51:50 -0400

    The map editor
    Added 2 new files map_editor.cpp and map_editor.h, Some errors exist

---
+++ map_editor.h
@@ -0,0 +1,33 @@
+//
+//  map_editor.h
+//  COMP345-A2
+//
+//  Created by Leonard Nguyen on 2015-02-24.
+//  Copyright (c) 2015 Leonard Nguyen. All rights reserved.
+//
+
+#ifndef __COMP345_A2__map_editor__
+#define __COMP345_A2__map_editor__
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include "map.h"
+
+//Is a friend of Map
+
+class MapEditor {
+    
+public:
+    MapEditor();
+    static void introMapEditor();
+    static void theMapEditor();
+    static void CountryCreator();
+    static void AdjacencyDefiner();
+    static void ContinentCreator();
+
+    
+    
+};
+
+#endif /* defined(__COMP345_A2__map_editor__) */
