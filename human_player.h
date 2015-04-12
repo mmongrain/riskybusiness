f590c51f8befcb9825c6c841ae410a4d47171c07
Matthew Mongrain <matthew.mongrain@gmail.com>
2015-04-11 20:52:28 -0400

    Minor fixes 	modified:   human_player.h

--- human_player.h
+++ human_player.h
@@ -1,25 +1,18 @@
 #ifndef HUMAN_PLAYER_H_
 #define HUMAN_PLAYER_H_
 
-#include "player.h"
-#include "territory.h"
+class Player;
 
 class HumanPlayer : public Player {
 
 public:
 	HumanPlayer() {}
 	~HumanPlayer() {}
-	bool WantsToAutoAttack();
-	void CaptureTerritory(Territory* attacking, Territory* defending, int min, int max);
-	int NumConqueringArmiesToMove(int min, int max);
 
 protected:
 	void Reinforce();
 	void Attack();
 	void Fortify();
-
-private:
-	bool AttackIsValid(Territory *attacking, std::string s);
 };
 
 
