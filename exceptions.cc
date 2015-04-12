c2fdd08dd5530e61a2199971fccdc4228de3788d
Matthew Mongrain <matthew.mongrain@gmail.com>
2015-04-12 16:04:59 -0400

    Bugfixes 	renamed:    exceptions.cpp -> exceptions.cc 	modified:   exceptions.h 	modified:   makefile

--- exceptions.cc
+++ exceptions.cc
@@ -6,24 +6,24 @@
 
 BattleException::BattleException(){
 }
-const char * BattleException::what() const {
+const char * BattleException::what() const throw () {
 	return typeid(*this).name();
 }
 
 CardException::CardException(){
 }
-const char * CardException::what() const {
+const char * CardException::what() const throw () {
 	return typeid(*this).name();
 }
 
 CardMatchException::CardMatchException(){
 }
-const char * CardMatchException::what() const {
+const char * CardMatchException::what() const throw () {
 	return typeid(*this).name();
 }
 
 EmptyDeckException::EmptyDeckException(){
 }
-const char * EmptyDeckException::what() const {
+const char * EmptyDeckException::what() const throw() {
 	return typeid(*this).name();
 }
--- exceptions.h
+++ exceptions.h
@@ -6,25 +6,25 @@
 class BattleException : public std::exception {
 public:
 	BattleException();
-	const char * what() const;
+	const char * what() const throw();
 };
 
 class CardException : public std::exception {
 public:
 	CardException();
-	const char * what() const;
+	const char * what() const throw();
 };
 
 class CardMatchException : public std::exception {
 public:
 	CardMatchException();
-	const char * what() const;
+	const char * what() const throw();
 };
 
 class EmptyDeckException : public std::exception {
 public:
 	EmptyDeckException();
-	const char * what() const;
+	const char * what() const throw();
 };
 
 
